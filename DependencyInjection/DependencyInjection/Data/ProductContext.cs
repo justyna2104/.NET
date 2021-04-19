bf3aa9edc7d13a704d880e3e3f77180c3538b07e
justyna2104 <justynaharmuszkiewicz@gmail.com>
2021-04-19 19:16:33 +0200

    Connection to Database.

--- DependencyInjection/DependencyInjection/Data/ProductContext.cs
+++ DependencyInjection/DependencyInjection/Data/ProductContext.cs
@@ -9,6 +9,8 @@ namespace DependencyInjection.Data
 {
     public class ProductContext : DbContext
     {
+        public ProductContext(DbContextOptions options) : base(options) { }
+
         public DbSet<Product> Product { get; set; }
     }
 }
--- DependencyInjection/DependencyInjection/Startup.cs
+++ DependencyInjection/DependencyInjection/Startup.cs
@@ -1,7 +1,9 @@
+using DependencyInjection.Data;
 using DependencyInjection.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,12 @@ namespace DependencyInjection
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddDbContext<ProductContext>(options => {
+                options.UseSqlServer(Configuration.GetConnectionString("DependencyInjectionDB"));
+            });
+
+
             services.AddRazorPages();
             services.AddTransient<JsonFileProductService>(); 
         }
