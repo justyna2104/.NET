a9698472110c117c6d822cf7e5339cac336cfc04
justyna2104 <justynaharmuszkiewicz@gmail.com>
2021-04-26 20:18:48 +0200

    My own middleware

---
+++ Middleware/Middleware/MyMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Middleware
+{
+    public class MyMiddleware
+    {
+        private RequestDelegate _next;
+
+        public MyMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext context)
+        {
+            var userAgent = context.Request.Headers["User-Agent"][0].ToString();
+
+            if (userAgent.Contains("Edge") || userAgent.Contains("Edg/") || userAgent.Contains("Trident/")) // Edge, EdgeChromium i IE
+            {
+                return context.Response.WriteAsync("Przegladarka nie jest obslugiwana");
+            }
+            else return _next(context);
+        }
+    }
+    }
+
